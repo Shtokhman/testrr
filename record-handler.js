c84ba02a1a88f32eb36b0472d34de4bb8cd0c8cd
Yuriy <36294649+Shtokhman@users.noreply.github.com>
2021-05-28 18:25:31 +0300

    add generating html feature

--- record-handler.js
+++ record-handler.js
@@ -1,19 +1,34 @@
 'use strict';
+
 const recorder = new ScriptRecorder();
 let result = null;
 recorder.start();
+
+function generateHtml(replayEvents, replaySpeed=1) {
+    return `<!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport"
+                      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                <title>Recording</title>
+            </head>
+            <body>
+            <div id="root"></div>
+            <script src="./script-rrweb.js"></script>
+            <script>var recording = ${JSON.stringify(replayEvents)}; var replayer = new rrweb.Replayer(recording, {speed: ${replaySpeed}, root: document.getElementById('root'), mouseTail: false}); replayer.play()</script>
+            </body>
+            </html>`;
+}
 
 let round = 0;
 document.querySelector('input[type=submit][value="Submit"]').addEventListener('click', () => {
     round++;
     if (round === 10) {
-        result = recorder.getEvents();
-        console.log('recorded events = ', result.length);
         recorder.stop();
+        result = recorder.getEvents();
+        window.result = generateHtml(result.map(e => e.event));
 
-        const replayer = new rrweb.Replayer(recorder.getEvents().map(e => e.event));
-        replayer.play();
-        replayer.setSpeed(0.5);
     }
     console.log(`Round - ${round}`)
 })
