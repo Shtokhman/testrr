0422ce37a95fef270c44421899a109d32ebb1fd2
Yuriy <36294649+Shtokhman@users.noreply.github.com>
2021-06-08 19:09:51 +0300

    Update test-record-handler.js

--- test-record-handler.js
+++ test-record-handler.js
@@ -37,9 +37,9 @@ document.querySelector('button.waves-effect.col.s12.m12.l12.btn-large.uiColorBut
 // })
 document.addEventListener("keydown", function(event) {
     console.log("Pressed");
-    console.log(event.shiftKey);
     console.log(event.altKey);
     console.log(event.key === 'b');
+    console.log(shiftKey && event.key === 'b');
 
 
     if (event.shiftKey && event.key === 'b') {
